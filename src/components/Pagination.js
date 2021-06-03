8212a6fae7ae2a1fe2bb06c33912fb70e970cda1
Apurav <apuravporiya10@gmail.com>
2021-06-03 17:50:15 +0530

    added new folder src

---
+++ src/components/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Pagination({gotoNextPage , gotoPrevPage}) {
+    return (
+        <div>
+          { gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button> }
+          { gotoNextPage && <button onClick={gotoNextPage}>Next</button> }
+        </div>
+    )
+}
+
+export default Pagination
